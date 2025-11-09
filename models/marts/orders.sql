13bb66ab95fc01127659f4a5dc2831fed7bb6d73
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2025-11-09 15:41:28 +0000

    reorganisation

---
+++ models/marts/orders.sql
@@ -0,0 +1,24 @@
+with order_payment as (
+    select 
+        order_id,
+        sum(amount) as total_amount
+    from {{ ref('stg_payments') }}
+    where payment_status <> 'fail'
+    group by order_id
+), order as (
+    select 
+        order_id,
+        customer_id,
+        order_date,
+        status as order_status
+    from {{ ref('stg_orders') }}
+)
+select 
+    order.order_id,
+    order.customer_id,
+    order.order_date,
+    order.order_status,
+    order_payment.total_amount
+from order
+left join order_payment
+on order.order_id = order_payment.order_id
---
+++ models/stg/stg_payments.sql
@@ -0,0 +1,8 @@
+select
+    id as line_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status as payment_status,
+    amount,
+    created
+from default.stripe_payments
